79068ed19733b38bb0e3612664b783e70398fa09
yus04 <yuswim192@gmail.com>
2022-12-24 14:24:20 +0900

    console.log(context);

--- api/HttpTrigger1/index.js
+++ api/HttpTrigger1/index.js
@@ -1,5 +1,20 @@
-module.exports = async function (context, req) {
+module.exports = async function (context) {
+    console.log(context);
     context.res.json({
         text: "Hello from the API"
     });
 };
+
+function show_time(){
+    return 10;
+}
+
+const date = new Date(); //現在時刻を取得
+const y = date.getFullYear(), //西暦年
+      m = date.getMonth() + 1, //月　
+      d = date.getDate(), //日
+      H = date.getHours(), //時
+      M = date.getMinutes(), //分
+      S = date.getSeconds(); //秒
+
+console.log(y,m,d,H,M,S); //例 2022 1 14 3 10 30
